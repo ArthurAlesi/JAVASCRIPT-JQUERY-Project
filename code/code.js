d2ccd8202215eab74e5e7503a14770bac6f607ce
ArthurAlesi <54421573+ArthurAlesi@users.noreply.github.com>
2021-11-16 14:40:26 -0300

    update

--- code/code.js
+++ code/code.js
@@ -1,17 +1,36 @@
+// functions 
+
+
+function createBoard() {
+    var num = 0;
+    for (var i = 1; i <= 50; i++) {
+
+        $(".board").append(`<tr class="row" id="r${i}"></tr>`)
+        for (var j = 1; j <= 50; j++) {
+            $(`#r${i}`).append(`<td class="cell" id="col${i}${j} row_number=${i} col_number=${j}"></td>`)    
+            $(`#${i}${j}`).attr("rownumber",i)
+            $(`#${i}${j}`).attr("colnumber",i)
+        }
+    }
+    console.log("fim create board")
+  
+    
+
+}
+
+
+
 // create board
 
 $(document).ready(function() {
     console.log("Ola mundo")
+    console.log("Ola mundo")
+    console.log("Ola mundo")
+    createBoard()
 });
 
-function createBoard() {
-    for (var i = 0; i <= 50; i++) {
-
-    }
-}
-
 // create snake
-
+snake = {"head": (25,25)}
 // set apple
 
 // run game
